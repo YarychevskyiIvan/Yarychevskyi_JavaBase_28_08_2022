d2db4d6a150aaa5ca65cdba006080ee1a3a4e727
Yarychevskyi Ivan <Melnykivan94@gmail.com>
2022-09-08 12:40:05 +0300

    lesson4

--- src/main/java/com/hillel/yarychevskyi/lessons/lesson4/MaApp.java
+++ src/main/java/com/hillel/yarychevskyi/lessons/lesson4/MaApp.java
@@ -1,4 +1,23 @@
 package com.hillel.yarychevskyi.lessons.lesson4;
 
 public class MaApp {
+    public static void main(String[] args) {
+
+        int warrior = 13;
+        int archer = 24;
+        int rider = 46;
+        int liSoldiers = 860;
+        int liWarriors = warrior * liSoldiers;
+        int LiArcher = archer * liSoldiers;
+        int LiRider = rider * liSoldiers;
+
+        System.out.println(liWarriors);
+        System.out.println(LiArcher);
+        System.out.println(LiRider);
+
+
+        double Min = liSoldiers * 1.5;
+
+
+    }
 }
