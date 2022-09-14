d4243665dc86e552ec66c16c70b43ece500f9b81
Yarychevskyi Ivan <Melnykivan94@gmail.com>
2022-09-14 19:04:48 +0300

    lesson 5 NEW GODE

--- src/main/java/com/hillel/yarychevskyi/lessons/lesson5/MyApp.java
+++ src/main/java/com/hillel/yarychevskyi/lessons/lesson5/MyApp.java
@@ -8,7 +8,7 @@ public class MyApp {
         System.out.println("Pleas enter name team1");
         String Team1 = scanner.nextLine();
         System.out.println("team1 = " + " " + Team1);
-        System.out.println( "Pleas enter frag for" + " " + Team1);
+        System.out.println("Pleas enter frag for" + " " + Team1);
 
         int bot1 = scanner.nextInt();
         int bot2 = scanner.nextInt();
@@ -19,7 +19,7 @@ public class MyApp {
         System.out.println("Pleas enter name team2");
         String Team2 = scanner.nextLine();
         System.out.println("Team2 = " + " " + Team2);
-        System.out.println( "Pleas enter frag for" + " " + Team2);
+        System.out.println("Pleas enter frag for" + " " + Team2);
 
         int teror1 = scanner.nextInt();
         int teror2 = scanner.nextInt();
@@ -27,9 +27,9 @@ public class MyApp {
         int teror4 = scanner.nextInt();
         int teror5 = scanner.nextInt();
         scanner.nextLine();
-        double OurBot = ((bot1 + bot2 + bot3 + bot4 + bot5)/5);
+        double OurBot = ((bot1 + bot2 + bot3 + bot4 + bot5) / 5);
         System.out.println("Середнє арифметичне" + " " + "команди" + " " + Team1 + " " + OurBot);
-        double OurTeror = ((teror1+teror2+teror3+teror4+teror5)/5);
+        double OurTeror = ((teror1 + teror2 + teror3 + teror4 + teror5) / 5);
         System.out.println("Середнє арифметичне" + " " + "команди" + " " + Team2 + " " + OurTeror);
         if (OurBot > OurTeror) {
             System.out.println("Перемогла команда" + " " + Team1 + " " + "набарала" + " " + OurBot + " " + "очків");
