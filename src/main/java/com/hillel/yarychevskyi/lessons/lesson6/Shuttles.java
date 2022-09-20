ab616a15b78004c3ed4e194d9de39159b261ba38
Yarychevskyi Ivan <Melnykivan94@gmail.com>
2022-09-20 19:57:29 +0300

    change code

--- src/main/java/com/hillel/yarychevskyi/lessons/lesson6/Shuttles.java
+++ src/main/java/com/hillel/yarychevskyi/lessons/lesson6/Shuttles.java
@@ -3,15 +3,18 @@ package com.hillel.yarychevskyi.lessons.lesson6;
 public class Shuttles {
     public static void main(String[] args) {
         int counter = 0;
-        for (int i = 1; i < 146; i++) {
-            if ((i == 4) || (i == 9) || (i / 10 == 4) || (i / 10 == 9) || (i % 10 == 4) || (i % 10 == 9)) {
+        int i;
+        for (i = 1; i < 160; i++) {
+            if ((i == 4) || (i == 9) || (i / 10 == 4) || (i / 10 == 9) || (i % 10 == 4) || (i % 10 == 9) || (i / 10 % 10 == 4) || (i / 10 % 10 == 9)) {
                 continue;
             }
             System.out.println("shuttle number:" + i);
             counter++;
+
         }
-
-        System.out.println("count = " + counter);
-
+           System.out.println("count = "+counter);
     }
+
+
+
 }
---
+++ src/main/java/com/hillel/yarychevskyi/lessons/lesson7/MyApp.java
@@ -0,0 +1,7 @@
+package com.hillel.yarychevskyi.lessons.lesson7;
+
+public class MyApp {
+    public static void main(String[] args) {
+
+    }
+}
