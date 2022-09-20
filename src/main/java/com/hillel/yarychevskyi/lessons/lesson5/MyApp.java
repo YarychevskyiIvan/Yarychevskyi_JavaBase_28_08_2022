e8d83418b25ad606ef69067b2d02a2b2f4b0a6a8
Yarychevskyi Ivan <Melnykivan94@gmail.com>
2022-09-20 18:20:10 +0300

    change code

--- src/main/java/com/hillel/yarychevskyi/lessons/lesson5/MyApp.java
+++ src/main/java/com/hillel/yarychevskyi/lessons/lesson5/MyApp.java
@@ -9,33 +9,53 @@ public class MyApp {
         String team1 = scanner.nextLine();
         System.out.println("team1 = " + " " + team1);
         System.out.println("Pleas enter frag for" + " " + team1);
+        int bot1 = 0;
+        int bot2 = 0;
+        int bot3 = 0;
+        int bot4 = 0;
+        int bot5 = 0;
+        if (scanner.hasNextInt()) {
+            bot1 = scanner.nextInt();
+            bot2 = scanner.nextInt();
+            bot3 = scanner.nextInt();
+            bot4 = scanner.nextInt();
+            bot5 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Pleas try agin");
+            System.exit(0);
+        }
 
-        int bot1 = scanner.nextInt();
-        int bot2 = scanner.nextInt();
-        int bot3 = scanner.nextInt();
-        int bot4 = scanner.nextInt();
-        int bot5 = scanner.nextInt();
         scanner.nextLine();
 
         System.out.println("Pleas enter name team2");
         String team2 = scanner.nextLine();
         System.out.println("Team2 = " + " " + team2);
         System.out.println("Pleas enter frag for" + " " + team2);
+        int teror1 = 0;
+        int teror2 = 0;
+        int teror3 = 0;
+        int teror4 = 0;
+        int teror5 = 0;
 
-        int teror1 = scanner.nextInt();
-        int teror2 = scanner.nextInt();
-        int teror3 = scanner.nextInt();
-        int teror4 = scanner.nextInt();
-        int teror5 = scanner.nextInt();
+        if (scanner.hasNextInt()) {
+            teror1 = scanner.nextInt();
+            teror2 = scanner.nextInt();
+            teror3 = scanner.nextInt();
+            teror4 = scanner.nextInt();
+            teror5 = scanner.nextInt();
+        } else {
+            System.out.println("Wrong data! Pleas try agin");
+            System.exit(0);
+        }
         scanner.nextLine();
         double ourbot = ((bot1 + bot2 + bot3 + bot4 + bot5) / 5);
         System.out.println("Середнє арифметичне" + " " + "команди" + " " + team1 + " " + ourbot);
         double ourteror = ((teror1 + teror2 + teror3 + teror4 + teror5) / 5);
         System.out.println("Середнє арифметичне" + " " + "команди" + " " + team2 + " " + ourteror);
         if (ourbot > ourteror) {
-            System.out.println("Перемогла команда" + " " + team1+ " " + "набарала" + " " + ourbot + " " + "очків");
+            System.out.println("Перемогла команда" + " " + team1 + " " + "набарала" + " " + ourbot + " " + "очків");
         } else if (ourbot < ourteror) {
-            System.out.println("Перемогла команда" + " " + team2 + " " + "набарала" + " " + ourteror+ " " + "очків");
+            System.out.println("Перемогла команда" + " " + team2 + " " + "набарала" + " " + ourteror + " " + "очків");
         } else {
             System.out.println("Нічія" + " " + "будь-ласка переграйте");
             scanner.close();
