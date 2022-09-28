d1e2c4420c3dffaef039ff7a90c31b4e88a8ae10
Yarychevskyi Ivan <Melnykivan94@gmail.com>
2022-09-28 18:33:56 +0300

    new code

--- src/main/java/com/hillel/yarychevskyi/lessons/lesson8/main.java
+++ src/main/java/com/hillel/yarychevskyi/lessons/lesson8/main.java
@@ -6,13 +6,13 @@ public class main {
     public static void main(String[] args) {
         int[] mass1 = new int[7];
         int[] mass2 = new int[7];
+        int counter = 0;
+        int temp = 0;
 
         for (int i = 0; i < mass1.length; i++) {
             mass1[i] = (int) (Math.random() * 10);
             mass2[i] = (int) (Math.random() * 10);
         }
-        int counter = 0;
-        int temp = 0;
         for (int i = 0; i < mass1.length - 1; i++) {
             for (int j = 0; j < mass1.length - 1 - i; j++) {
                 counter++;
@@ -23,11 +23,11 @@ public class main {
                 }
             }
         }
-        int counter2 = 0;
-        int temp2 = 0;
+
+
         for (int i = 0; i < mass2.length - 1; i++) {
             for (int j = 0; j < mass2.length - 1 - i; j++) {
-                counter2++;
+                counter++;
                 if (mass2[j] > mass2[j + 1]) {
                     temp = mass2[j];
                     mass2[j] = mass2[j + 1];
@@ -35,14 +35,24 @@ public class main {
                 }
             }
 
+        }
+        boolean numbers = false;
+        counter = 0;
+        for (int j = 0; j < mass1.length; j++) {
+            if (mass1[j] == mass2[j]) {
+                counter++;
 
-
-
-        } System.out.println(Arrays.toString(mass1));
+            } else {
+                numbers = mass1[j] != mass2[j];
+                continue;
+            }
+        }
+        System.out.println(Arrays.toString(mass1));
         System.out.println(Arrays.toString(mass2));
 
-
+        System.out.println("Кількість збігів = " + counter);
 
 
     }
+
 }
