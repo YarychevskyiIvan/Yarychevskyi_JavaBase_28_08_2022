c6108b556ff1256aa663e66f4e48577ba72f9a15
Yarychevskyi Ivan <Melnykivan94@gmail.com>
2022-10-03 20:29:21 +0300

    New

---
+++ src/main/java/com/hillel/yarychevskyi/lessons/lessons10/Person.java
@@ -0,0 +1,16 @@
+package com.hillel.yarychevskyi.lessons.lessons10;
+
+public class Person {
+    public static void main(String[] args) {
+        System.out.println(personInfo("Will", "Smith", "New York", "2936729462846"));
+        System.out.println(personInfo("Dwayne", "Johnson", "California", "12312412412"));
+        System.out.println(personInfo("Adam", "Sandler", "Brooklyn", "37742123513"));
+
+
+    }
+
+    static String personInfo(String name, String surname, String city, String phone) {
+        String str = "Зателефонувати громадянину " + " " + name + " " + surname + " із міста " + city + " можна за номером " + phone;
+        return str;
+    }
+}
