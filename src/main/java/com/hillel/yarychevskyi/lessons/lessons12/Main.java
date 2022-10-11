30a429c53a39604a7cc98664569ad88bc3de82dd
Yarychevskyi Ivan <Melnykivan94@gmail.com>
2022-10-11 20:23:02 +0300

    add another code

---
+++ src/main/java/com/hillel/yarychevskyi/lessons/lessons12/Main.java
@@ -0,0 +1,24 @@
+package com.hillel.yarychevskyi.lessons.lessons12;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Tracker tracker = new Tracker("Oleg", 25, 7,1994, "Olegz@gmail.com", "0671739001","Lomazhyk", 95.4, 80, 5000);
+        tracker.printAccountInfo();
+        tracker.setSteps(5550);
+        tracker.printAccountInfo();
+        tracker.setSurname("Verba");
+        tracker.printAccountInfo();
+        System.out.println();
+        Tracker tracker1 = new Tracker("Roman",5,9, 1989,"Roman@gmail.com", "80971214563","Kovalenko", 101,75,1233);
+        tracker1.printAccountInfo();
+        tracker1.setSteps(2541);
+        tracker1.printAccountInfo();
+        System.out.println();
+        Tracker tracker2 = new Tracker("Viktor", 25,10,1984, "Vikorr@gmail.com", "0631456987", "Radchenko", 89.4,65,7899);
+        tracker2.printAccountInfo();
+        tracker2.setWeight(96.8);
+        tracker2.printAccountInfo();
+    }
+
+}
---
+++ src/main/java/com/hillel/yarychevskyi/lessons/lessons12/Tracker.java
@@ -0,0 +1,92 @@
+package com.hillel.yarychevskyi.lessons.lessons12;
+
+public class Tracker {
+    private final String name;
+    private final int  birthDay;
+    private final int birthMount;
+    private final int birthYear;
+    private final String email;
+    private final String phoneNumber;
+    private String surname;
+    private  double weight;
+    private double pressure;
+    private double steps;
+    private int age;
+
+    public Tracker(String name, int birthDay, int birthMount, int birthYear, String email, String phoneNumber, String surname, double weight, double pressure, double steps) {
+        this.name = name;
+        this.birthDay = birthDay;
+        this.birthMount = birthMount;
+        this.birthYear = birthYear;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.surname = surname;
+        this.weight = weight;
+        this.pressure = pressure;
+        this.steps = steps;
+
+
+        age = 2020 - birthYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBirthDay() {
+        return birthDay;
+    }
+
+    public int getBirthMount() {
+        return birthMount;
+    }
+
+    public int getBirthYear() {
+        return birthYear;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public void setSteps(double steps) {
+        this.steps = steps;
+    }
+public void printAccountInfo() {
+    System.out.println (
+            "name='" + name + '\'' +
+            ", birthDay=" + birthDay +
+            ", birthMount=" + birthMount +
+            ", birthYear=" + birthYear +
+            ", email='" + email + '\'' +
+            ", phoneNumber='" + phoneNumber + '\'' +
+            ", surname='" + surname + '\'' +
+            ", weight=" + weight +
+            ", pressure=" + pressure +
+            ", steps=" + steps +
+            ", age=" + age
+            );
+}
+
+
+}
+
+
+
+
